3e5d98c38d36db001c1c9cf7d132fb2200eb01c7
Surasith <surasith.kae@ascendcorp.com>
2025-10-20 02:22:11 +0700

    wip: add a simple web api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{debug_handler, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(todo_get));
+
+    let addr = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    println!("Listening on http://{}", addr);
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Serialize, Deserialize)]
+struct TodoItem {
+    id: u32,
+    title: String,
+    completed: bool,
+}
+
+#[debug_handler]
+async fn todo_get() -> Json<TodoItem> {
+    Json::from(TodoItem {
+        id: 1,
+        title: "Learn Rust".to_string(),
+        completed: false,
+    })
 }

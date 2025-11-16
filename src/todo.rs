780f71233c56e2e2d37c313fc5bcd7c75ca95301
Surasith <surasith.kae@ascendcorp.com>
2025-11-16 19:30:48 +0700

    wip: add headers response

--- src/todo.rs
+++ src/todo.rs
@@ -1,12 +1,14 @@
 use axum::{
     debug_handler,
     extract::{Path, State},
-    http::StatusCode,
+    http::{header, StatusCode},
+    response::IntoResponse,
     routing::{delete, get, patch, post},
     Json, Router,
 };
+use httpdate::HttpDate;
 use serde::{Deserialize, Serialize};
-use std::sync::Arc;
+use std::{sync::Arc, time};
 use tokio::sync::Mutex;
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -14,6 +16,8 @@ pub struct TodoItem {
     id: Option<String>,
     title: String,
     completed: bool,
+    created_at: time::SystemTime,
+    updated_at: time::SystemTime,
 }
 
 // NOTE: In a real application, use proper synchronization (e.g., Mutex) for shared state.
@@ -52,27 +56,48 @@ pub struct CreateTodoItem {
 async fn create_handler(
     State(store): State<TodoStore>,
     Json(new_item): Json<CreateTodoItem>,
-) -> Result<Json<TodoItem>, StatusCode> {
+) -> impl IntoResponse {
     let item = TodoItem {
         id: Some(uuid::Uuid::new_v4().to_string()),
         title: new_item.title.clone(),
         completed: new_item.completed,
+        created_at: time::SystemTime::now(),
+        updated_at: time::SystemTime::now(),
     };
 
     store.lock().await.push(item.clone());
-    Ok(Json::from(item))
+
+    let location = format!("/todos/{}", item.id.as_ref().unwrap());
+    IntoResponse::into_response((
+        StatusCode::CREATED,
+        [(header::LOCATION, location)],
+        Json(item.clone()),
+    ))
 }
 
 #[debug_handler]
-async fn get_handler(
-    State(store): State<TodoStore>,
-    Path(id): Path<String>,
-) -> Result<Json<TodoItem>, StatusCode> {
+async fn get_handler(State(store): State<TodoStore>, Path(id): Path<String>) -> impl IntoResponse {
     let items = store.lock().await;
     let item = items.iter().find(|item| item.id.as_ref() == Some(&id));
-    match item {
-        Some(item) => Ok(Json(item.clone())),
-        None => Err(StatusCode::NOT_FOUND),
+
+    if let Some(item) = item {
+        // format the SystemTime as HTTP date
+        let last_modified: HttpDate = item.updated_at.into();
+        let etag = item
+            .updated_at
+            .duration_since(time::UNIX_EPOCH)
+            .unwrap()
+            .as_nanos();
+        IntoResponse::into_response((
+            StatusCode::OK,
+            [
+                (header::LAST_MODIFIED, last_modified.to_string().as_str()),
+                (header::ETAG, format!("{etag}").as_str()),
+            ],
+            Json(item.clone()),
+        ))
+    } else {
+        IntoResponse::into_response(StatusCode::NOT_FOUND)
     }
 }
 
@@ -88,6 +113,8 @@ async fn patch_handler(
     Path(id): Path<String>,
     Json(update): Json<PatchTodoItem>,
 ) -> Result<Json<TodoItem>, StatusCode> {
+    // TODO: check if header If-Match or If-Unmodified-Since matches
+
     let mut items = store.lock().await;
     if let Some(item) = items.iter_mut().find(|item| item.id.as_ref() == Some(&id)) {
         if let Some(title) = update.title {
